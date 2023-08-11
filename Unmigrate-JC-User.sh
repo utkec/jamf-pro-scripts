240b516b46682f9608b0a4c5030219932aa86e08
Casey <utkec@users.noreply.github.com>
2023-08-11 14:26:58 -0500

    Update Unmigrate-JC-User.sh

--- Unmigrate-JC-User.sh
+++ Unmigrate-JC-User.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
-#Get currently logged in user
-getUser=$(ls -l /dev/console | awk '{ print $3 }')
- 
-#Get alias of currently logged in user ($getUser)
-alias=$(dscl . read /Users/$getUser RecordName | awk '{ print $3 }')
+# Unmigrate all users from Jamf Connect
 
-# Double check user and alias
-echo "$getUser"
-echo "$alias"
-
-# Delete all the Jamf Connect mappings
-echo "Delete RecordName"
-echo "-------------"
-dscl . delete /Users/$getUser RecordName $alias
-echo "-------------"
-echo "Delete NetworkUser"
-echo "-------------"
-dscl . delete /Users/$getUser dsAttrTypeStandard:NetworkUser
-echo "-------------"
-echo "Delete OIDCProvider"
-echo "-------------"
-dscl . delete /Users/$getUser dsAttrTypeStandard:OIDCProvider
-echo "-------------"
-echo "Delete OktaUser"
-echo "-------------"
-dscl . delete /Users/$getUser dsAttrTypeStandard:OktaUser
-echo "-------------"
+# For all users who have a password on this machine (eliminates service accounts
+# but includes the _mbsetupuser and Jamf management accounts...)
+for user in $(/usr/bin/dscl . list /Users Password | /usr/bin/awk '$2 != "*" {print $1}'); do
+        # If a user has the attribute "NetworkUser" in their user record, they are a Jamf Connect user.
+        MIGRATESTATUS=($(/usr/bin/dscl . -read /Users/$user | grep "NetworkUser: " | /usr/bin/awk {'print $2'}))
+        # If we didn't get a result, the variable is empty.  Thus that user is not 
+        # a Jamf Connect Login user.
+        if [[ -z $MIGRATESTATUS ]]; 
+        then
+                # user is not a jamf connect user
+                echo "$user is Not a Jamf Connect User"
+        else
+                dscl . delete /Users/$user dsAttrTypeStandard:NetworkUser
+                dscl . delete /Users/$user dsAttrTypeStandard:OIDCProvider
+                dscl . delete /Users/$user dsAttrTypeStandard:OktaUser
+                dscl . delete /Users/$user dsAttrTypeStandard:AzureUser
+        fi
+done
